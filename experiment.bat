9184b832bc013d094e8a56f403b33a91784652dc
OAndrija <andrija.obradovic@student.um.si>
2024-11-07 16:26:59 +0100

    updated script for testing with seed={1,...,50}

---
+++ experiment.bat
@@ -0,0 +1,16 @@
+@echo off
+
+:: Compile the C++ program with g++
+g++ -o experiment.exe main.cpp
+
+:: Check if the compilation was successful
+if %errorlevel% neq 0 (
+    echo Compilation failed.
+    exit /b %errorlevel%
+)
+
+:: Run the program with seed values from 1 to 50
+for /L %%i in (1,1,50) do (
+    echo Running experiment with seed %%i
+    experiment.exe ABBBBBBABBBAB -seed %%i -target -5.6104 -nfesLmt 1000000 -runtimeLmt 60.0 -Np 300
+)
